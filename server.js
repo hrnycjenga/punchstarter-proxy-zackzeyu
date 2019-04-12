916166c882ab8f69a0f3981f948e2a06c9985a2d
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-12 12:55:14 -0400

    add get request

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use('/:id', express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, './public'))
+})
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);

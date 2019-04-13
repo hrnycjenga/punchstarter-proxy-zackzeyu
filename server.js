f1a5c9c69419aa628cbdfd292c0980b881e020f5
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-13 15:45:48 -0400

    adjust cors settings

--- server.js
+++ server.js
@@ -7,54 +7,16 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(cors());
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/product/:id', (req, res) => {
-  var id = req.params.id;
-  axios
-    .get(`ec2-3-88-85-118.compute-1.amazonaws.com/product/${id}`)
-    .then(result => {
-      res.send(result.data);
-    })
-    .catch(err => console.log(err));
-});
-
-app.post('/product/:id', (req, res) => {
-  var id = req.params.id;
-  axios
-    .post(`ec2-3-88-85-118.compute-1.amazonaws.com/product/${id}`)
-    .then(result => {
-      res.send(result.data);
-    })
-    .catch(err => console.log(err));
-});
-
-app.post('/reward/:id/', (req, res) => {
-  var id = req.params.id;
-  axios
-    .post(`ec2-3-88-85-118.compute-1.amazonaws.com/reward/${id}`)
-    .then(result => {
-      res.send(result.data);
-    })
-    .catch(err => console.log(err));
-});
-
-app.get('/countries/:id', (req, res) => {
-  var id = req.params.id;
-  axios
-    .get(`ec2-3-88-85-118.compute-1.amazonaws.com/countries/${id}`)
-    .then(result => {
-      res.send(result.data);
-    })
-    .catch(err => console.log(err));
-});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);

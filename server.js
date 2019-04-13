654863b5eb63538e0ffa185668210cf0237e9fda
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-13 10:24:23 -0400

    reorder cors in server

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const app = express();
-
-app.use(express.static(path.join(__dirname, './public')));
-app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, './public'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(port, function() {

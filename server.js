14e38e14a8ca6e16a7ec600f477b90125803384d
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-11 11:41:27 -0400

    (init) With basic modifications for updated component

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
-const axios = require('axios');
 const cors = require('cors');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3400;
 const app = express();
 
 app.use(morgan('dev'));
@@ -14,10 +13,9 @@ app.use('/:id', express.static(path.join(__dirname, '/public')));
 app.use(cors());
 
 app.get('/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-
 app.listen(port, function() {
-  console.log(`listening on port ${port}`);
+	console.log(`listening on port ${port}`);
 });

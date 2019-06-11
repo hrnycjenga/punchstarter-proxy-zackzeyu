c53ca73794c6231e47c2cf102053404605f7278c
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-11 12:14:35 -0400

    Remove unneeded depedencies

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
-const cors = require('cors');
 
 const port = process.env.PORT || 3400;
 const app = express();
@@ -10,7 +9,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/:id', express.static(path.join(__dirname, '/public')));
-app.use(cors());
 
 app.get('/:id', (req, res) => {
 	res.sendFile(path.join(__dirname, '/public/index.html'));

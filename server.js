2ed354c9ce0f3f8279167d376e85f0873caa0932
Zack Zhu <zeyuzhu1@gmail.com>
2019-06-11 14:39:52 -0400

    Fix serving static files

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 // const morgan = require('morgan');
 const path = require('path');
 
@@ -7,12 +6,8 @@ const port = process.env.PORT || 3400;
 const app = express();
 
 // app.use(morgan('dev'));
-// app.use(bodyParser.json());
-app.use('/:id', express.static(path.join(__dirname, '/public')));
-
-app.get('/:id', (req, res) => {
-	res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.use('/static', express.static(path.join(__dirname, 'public/static')));
+app.use('/:id', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, function() {
 	console.log(`🍿 Proxy server listening on port ${port}`);
